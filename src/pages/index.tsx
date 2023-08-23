3d35e4dec08874a7f5bb089b3f2755740b92621e
proistak <manos.proistakis@sword-group.com>
2023-08-23 14:13:42 +0300

    Collapsable story floating container

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,20 @@
-import React, { useState, useRef } from 'react';
-import { Button, ButtonGroup } from '@chakra-ui/react';
+import React, { useState, useRef, ReactNode } from 'react';
+import {
+  Box,
+  Button,
+  ButtonGroup,
+  Collapse,
+  Text,
+  useDisclosure,
+} from '@chakra-ui/react';
 import Image from 'next/image';
 
 const HorrorPage = () => {
   const prompt = `Generate a horror story`;
+  // const prompt = `Hello`;
   const [story, setStory] = useState('');
   const [isLoading, setLoading] = useState(false);
+  const { isOpen, onToggle } = useDisclosure();
 
   const myRef = useRef<null | HTMLDivElement>(null);
 
@@ -17,6 +26,7 @@ const HorrorPage = () => {
 
   const generateStory = async (e: any) => {
     e.preventDefault();
+    onToggle();
     setStory('');
     setLoading(true);
     const response = await fetch('/api/generate', {
@@ -56,6 +66,9 @@ const HorrorPage = () => {
     }
     scrollToHeader();
     setLoading(false);
+    // setStory(
+    //   "As they wandered through the mansion's labyrinthine corridors, the adventurers began to hear faint, chilling whispers that seemed to come from the very walls themselves. The whispers spoke of forgotten sorrows, ancient regrets, and a hunger for release. Goosebumps formed on their skin, and a growing sense of unease gnawed at their hearts. Despite their fear, the group pressed on, driven by a morbid curiosity. They discovered a hidden chamber, and in its center stood an ornate pedestal holding the unearthly relic—a small, obsidian figurine pulsating with an eerie light. Its unsettling aura seemed to warp reality around it. As they wandered through the mansion's labyrinthine corridors, the adventurers began to hear faint, chilling whispers that seemed to come from the very walls themselves. The whispers spoke of forgotten sorrows, ancient regrets, and a hunger for release. Goosebumps formed on their skin, and a growing sense of unease gnawed at their hearts. Despite their fear, the group pressed on, driven by a morbid curiosity. They discovered a hidden chamber, and in its center stood an ornate pedestal holding the unearthly relic—a small, obsidian figurine pulsating with an eerie light. Its unsettling aura seemed to warp reality around it.  As they wandered through the mansion's labyrinthine corridors, the adventurers began to hear faint, chilling whispers that seemed to come from the very walls themselves. The whispers spoke of forgotten sorrows, ancient regrets, and a hunger for release. Goosebumps formed on their skin, and a growing sense of unease gnawed at their hearts. Despite their fear, the group pressed on, driven by a morbid curiosity. They discovered a hidden chamber, and in its center stood an ornate pedestal holding the unearthly relic—a small, obsidian figurine pulsating with an eerie light. Its unsettling aura seemed to warp reality around it. As they wandered through the mansion's labyrinthine corridors, the adventurers began to hear faint, chilling whispers that seemed to come from the very walls themselves. The whispers spoke of forgotten sorrows, ancient regrets, and a hunger for release. Goosebumps formed on their skin, and a growing sense of unease gnawed at their hearts. Despite their fear, the group pressed on, driven by a morbid curiosity. They discovered a hidden chamber, and in its center stood an ornate pedestal holding the unearthly relic—a small, obsidian figurine pulsating with an eerie light. Its unsettling aura seemed to warp reality around it.  "
+    // );
   };
 
   return (
@@ -66,17 +79,27 @@ const HorrorPage = () => {
       <main className="horror-content">
         <Image
           src="/header-image.jpg" // Replace with your horror-themed image URL
-          alt="Horror"
+          alt="Horror Image"
           sizes="100vw"
           width={0}
           height={0}
-          style={{ width: '100%', height: 'auto' }} // optional
           className="horror-image"
         />
         <Button variant={'ghost'} onClick={(e) => generateStory(e)}>
           Tell me a story
         </Button>
-        {isLoading ? <p>Loading...</p> : <p>{story}</p>}
+        <Collapse in={isOpen} animateOpacity className="horror-story-container">
+          <Box
+            p="40px"
+            color="white"
+            mt="4"
+            bg="teal.800"
+            rounded="md"
+            shadow="md"
+          >
+            {isLoading && isOpen ? <Text fontSize="xl">Loading</Text> : story}
+          </Box>
+        </Collapse>
       </main>
       <footer className="horror-footer">
         <p>© {new Date().getFullYear()} Horror App. Powered by OpenAI.</p>
