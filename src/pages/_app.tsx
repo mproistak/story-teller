e4f73d94a07fe6a7714ec68b6ce0ababb0e56d59
proistak <manos.proistakis@sword-group.com>
2023-08-23 18:11:10 +0300

    Added squiggly effect to header title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,7 @@ import { ChakraProvider } from '@chakra-ui/react';
 import Layout from '../layout/Layout';
 import theme from '../theme';
 import '../styles/globals.css';
+import '../styles/index.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -61,7 +61,38 @@ const HorrorPage = () => {
   return (
     <div className="horror-page">
       <header className="horror-header">
-        <h1>Welcome to your daily horror story</h1>
+        <h1 className="horror-title">Welcome to your daily horror story</h1>
+        <div style={{ display: 'none' }}>
+          <svg xmlns="http://www.w3.org/2000/svg" version="1.1">
+            <defs>
+              <filter id="squiggly-0">
+                <feTurbulence
+                  id="turbulence"
+                  baseFrequency="0.02"
+                  numOctaves="3"
+                  result="noise"
+                  seed="0"
+                />
+                <feDisplacementMap
+                  id="displacement"
+                  in="SourceGraphic"
+                  in2="noise"
+                  scale="6"
+                />
+              </filter>
+              <filter id="squiggly-1">
+                <feTurbulence
+                  id="turbulence"
+                  baseFrequency="0.02"
+                  numOctaves="3"
+                  result="noise"
+                  seed="1"
+                />
+                <feDisplacementMap in="SourceGraphic" in2="noise" scale="8" />
+              </filter>
+            </defs>
+          </svg>
+        </div>
       </header>
       <main className="horror-content">
         <Box
