3f8f9fbaee67b2a5020ba8ef4327becaae022a4d
proistak <manos.proistakis@sword-group.com>
2023-08-29 10:03:52 +0300

    Enable story

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,10 +19,10 @@ const HorrorPage = ({ story }: StoryProps) => {
 };
 
 export async function getStaticProps() {
-  // const prompt = `Generate a horror story`;
-  const prompt = `Hello`;
-  // const story = await generateStory(prompt);
-  const story = 'Random text to not charge by hitting api :)';
+  const prompt = `Generate a horror story`;
+  // const prompt = `Hello`;
+  const story = await generateStory(prompt);
+  // const story = 'Random text to not charge by hitting api :)';
 
   return {
     props: {
