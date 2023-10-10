53a0e0b9fb8e9465574bfe1ea301f2d7b7fc28ec
proistak <manos.proistakis@sword-group.com>
2023-10-10 15:37:06 +0300

    Revalidate once per day

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -28,8 +28,8 @@ export async function getStaticProps() {
     props: {
       story,
     },
-    // revalidate: 86400, // Revalidate once per day
-    revalidate: 60, // Revalidate every one minute
+    revalidate: 86400, // Revalidate once per day
+    // revalidate: 60, // Revalidate every one minute
   };
 }
 
