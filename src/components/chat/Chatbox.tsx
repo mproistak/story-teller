76464510bf80575b531f1c60c9e1345bb16424d0
Manos Proistakis <140259699+mproistak@users.noreply.github.com>
2023-10-28 21:40:00 +0300

    Firebase chat (#5)

---
+++ src/components/chat/Chatbox.tsx
@@ -0,0 +1,65 @@
+import React, { useEffect, useRef, useState } from 'react';
+import {
+  query,
+  where,
+  collection,
+  orderBy,
+  onSnapshot,
+  limit,
+} from 'firebase/firestore';
+import { Box } from '@chakra-ui/react';
+import { auth, db } from '../../../firebase';
+import MessageComponent from './MessageComponent';
+import SendMessage from './SendMessage';
+import { Message } from 'src/types';
+
+const ChatBox = () => {
+  const [messages, setMessages] = useState<Message[]>([]);
+  const scroll = useRef<HTMLSpanElement>(null);
+
+  useEffect(() => {
+    if (!auth.currentUser) return;
+
+    const messageQuery = query(
+      collection(db, 'messages'),
+      where('currentUserUid', '==', auth.currentUser.uid),
+      orderBy('createdAt', 'desc'),
+      limit(50)
+    );
+
+    const unsubscribe = onSnapshot(messageQuery, (QuerySnapshot) => {
+      const fetchedMessages: any = [];
+      QuerySnapshot.forEach((doc) => {
+        fetchedMessages.push({ ...doc.data(), id: doc.id });
+      });
+      const sortedMessages = fetchedMessages.sort(
+        (a: any, b: any) => a.createdAt - b.createdAt
+      );
+      setMessages(sortedMessages);
+    });
+    return () => unsubscribe();
+  }, []);
+
+  return (
+    <Box
+      margin="0px"
+      color="white"
+      backgroundImage="/header-image-haloween.jpg"
+      className="horror-box"
+      rounded="md"
+    >
+      <main className="chat-box">
+        <div className="messages-wrapper">
+          {messages?.map((message: Message) => (
+            <MessageComponent key={message.createdAt} message={message} />
+          ))}
+          {/* when a new message enters the chat, the screen scrolls down to the scroll div */}
+          {scroll && <span ref={scroll}></span>}
+        </div>
+        <SendMessage scroll={scroll} />
+      </main>
+    </Box>
+  );
+};
+
+export default ChatBox;
---
+++ src/components/chat/SendMessage.tsx
@@ -0,0 +1,106 @@
+import React, { useEffect, useState } from 'react';
+import { addDoc, collection, serverTimestamp } from 'firebase/firestore';
+import { sendMessage } from '@utils/helpers';
+import { auth, db } from '../../../firebase';
+
+type SendMessageProps = {
+  scroll: React.RefObject<HTMLSpanElement>;
+};
+
+type OpenAiUser = {
+  name: string;
+  photoUrl: string;
+  uid: string;
+};
+
+const SendMessage = ({ scroll }: SendMessageProps) => {
+  const [message, setMessage] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const systemUser: OpenAiUser = {
+    name: 'Violet',
+    photoUrl: process.env.NEXT_PUBLIC_FIREBASE_OPENAI_PHOTO_URL ?? '',
+    uid: process.env.NEXT_PUBLIC_FIREBASE_OPENAI_USERID ?? '',
+  };
+
+  useEffect(() => {
+    scroll.current?.scrollIntoView();
+  }, [scroll.current]);
+
+  const sendUserMessage = async (event: any) => {
+    event.preventDefault();
+    if (message.trim() === '') {
+      alert('Enter valid message');
+      return;
+    }
+
+    if (!auth.currentUser) return;
+    setIsLoading(true);
+    const { uid, displayName, photoURL } = auth.currentUser;
+    await addDoc(collection(db, 'messages'), {
+      text: message,
+      name: displayName,
+      avatar: photoURL,
+      createdAt: serverTimestamp(),
+      currentUserUid: auth.currentUser.uid,
+      uid,
+    });
+
+    scroll.current?.scrollIntoView({ behavior: 'smooth' });
+
+    await sendOpenAIResponseMessage(message);
+
+    scroll.current?.scrollIntoView({ behavior: 'smooth' });
+    setMessage('');
+    setIsLoading(false);
+  };
+
+  const sendOpenAIResponseMessage = async (messageForReply: string) => {
+    if (message.trim() === '') {
+      alert('Enter valid message');
+      return;
+    }
+
+    const reply = await sendMessage([
+      {
+        content:
+          'You are Violet, a ghost that reluctantly answers questions with sarcastic responses',
+        role: 'system',
+      },
+      { content: messageForReply, role: 'user' },
+    ]);
+
+    await addDoc(collection(db, 'messages'), {
+      text: reply,
+      name: systemUser.name,
+      avatar: systemUser.photoUrl,
+      createdAt: serverTimestamp(),
+      uid: systemUser.uid,
+      currentUserUid: auth.currentUser?.uid,
+    });
+  };
+
+  return (
+    <form onSubmit={(event) => sendUserMessage(event)} className="send-message">
+      <div className="message-container">
+        {isLoading && <span>Violet is typing ...</span>}
+        <div className="message-input">
+          <label htmlFor="messageInput" hidden>
+            Enter Message
+          </label>
+          <input
+            id="messageInput"
+            name="messageInput"
+            type="text"
+            className="form-input__input"
+            placeholder="type message..."
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          />
+          <button type="submit">Send</button>
+        </div>
+      </div>
+    </form>
+  );
+};
+
+export default SendMessage;
